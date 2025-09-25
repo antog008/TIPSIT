0c6add0641db15acfe6e75b993081fa3c33eba82
AbbaWv3s <160223114+AbbaWv3s@users.noreply.github.com>
2025-09-25 12:37:07 +0200

    update file uno.cpp
    aggiunta della differenza

--- laboratorio/uno.cpp
+++ laboratorio/uno.cpp
@@ -5,9 +5,12 @@ int main()
     int num1;
     int num2;
     int somma;
+    int sott;
     cout<<"dichiara numeri"<<endl;
     cin>>num1;
     cin>>num2;
     somma=num1+num2;
     cout<<"la somma è  "<<somma<<endl;
+    sott=num1-num2;
+    cout<<"la differenza è  "<<sott<<endl;
 }
