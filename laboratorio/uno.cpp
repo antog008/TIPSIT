c487a9b798e19672fa493bf97855c1f3a2bfb226
AbbaWv3s <160223114+AbbaWv3s@users.noreply.github.com>
2025-09-25 12:34:29 +0200

    primo uno.cpp
    operazione:somma

---
+++ laboratorio/uno.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num1;
+    int num2;
+    int somma;
+    cout<<"dichiara numeri"<<endl;
+    cin>>num1;
+    cin>>num2;
+    somma=num1+num2;
+    cout<<"la somma è  "<<somma<<endl;
+}
